83145b933ba213178dcf421522df8ba8559c6165
Ryo Fujinami <68380197+RyoFuji2005@users.noreply.github.com>
2020-11-10 00:20:05 +0900

    Add files via upload

--- shilitori.js
+++ shilitori.js
@@ -1,23 +1,52 @@
 
-const clist = Object.keys(rdict)
-cwe = "リ"
-cword = "シリトリ"
-occur = []
-num = 1
+/*
+vdict: 母音辞書 (Vowel dict)
+sdict: 清音辞書 (Seione dict)
+wdict: 単語辞書 (Word dict)
+*/
 
+const clist = Object.keys(sdict) // 文字リスト (Character list)
+
+let cword = "シリトリ" // 機械側の単語 (Computer's word)
+let cwend = cword.slice(-1) // 機械側の単語の最後の文字 (End of computer's word)
+let used_words = [] // 使用された単語リスト (Used word list)
+let num_time = 1 // 現在の回数 (Number of times)
+
+window.addEventListener("keydown", keyDown); // キーが押された時の設定
+
+// キーが押された時の関数
+function keyDown(event) {
+    // Shiftなら実行
+    if (event.keyCode === 16) {
+        decide()
+    }
+}
+
+// 決定された時の関数
 function decide() {
-    let pword = document.getElementById("entry1").value
-    let lenp = pword.length
-    if (lenp === 0) {
+    let pword = document.getElementById("entry1").value // プレイヤー側の単語 (Player's word)
+    let lenpw = pword.length // プレイヤー側の単語の文字数 (Length of player's word)
+
+    // 文字数が０文字、１文字、10文字以上の時は警告を出して終了
+    if (lenpw === 0) {
         alert('入力してください')
         return
-    } else if (lenp === 1) {
+    } else if (lenpw === 1) {
         alert('１文字の単語は禁止です')
         return
-    } else if (lenp > 10) {
+    } else if (lenpw > 10) {
         alert('10文字より多い単語は禁止です')
+        return
     }
-    for (let i = 0; i < lenp; i++) {
+
+    // 今まで出ている単語の時は終了
+    if (used_words.indexOf(pword) !== -1) {
+        alert('その言葉はもう出ています')
+        return
+    }
+
+    // 認識できる文字か確認
+    for (let i = 0; i < lenpw; i++) {
         let char = pword[i]
         if (clist.indexOf(char) === -1) {
             if (char !== "ー") {
@@ -26,74 +55,86 @@ function decide() {
             }
         }
     }
-    pwf = rdict[pword.slice(0, 1)]
-    let pwe = pword.slice(-1)
-    if (pwf != cwe) {
+
+    let pwfirst = sdict[pword.slice(0, 1)] // プレイヤー側の単語の最初の文字 (First of player's word)
+    let pwend = pword.slice(-1) // プレイヤー側の単語の最後の文字 (End of player's word)
+
+    // 最初の文字と最後の文字を確認
+    if (pwfirst !== cwend) {
         alert('初めの文字を確認してください')
-    } else if (pwe === 'ン') {
+        return
+    } else if (pwend === 'ン') {
         alert('「ン」が付きました\nあなたの負けです')
         refresh()
         return
     }
+
+    // 最後の文字が長音符なら一文字前の母音を利用
     if (pword.slice(-1) === 'ー') {
-        pwe = vdict[pword.slice(-2, -1)]
+        pwend = vdict[pword.slice(-2, -1)]
     } else {
-        pwe = pword.slice(-1)
+        pwend = pword.slice(-1)
     }
-    pwe = rdict[pwe]
-    if (occur.indexOf(pword) !== -1) {
-        alert('その言葉はもう出ています')
-    } else if (pwf === cwe) {
-        occur.push(pword)
-        if (wdict[cwe].indexOf(pword)) {
-            delete wdict[cwe][pword]
-        }
-        if (wdict[pwe].length === 0) {
-            alert('もう言葉を思いつきません\nあなたの勝ちです')
-            refresh()
-            return
-        }
-        cword = wdict[pwe][Math.floor(Math.random() * wdict[pwe].length)]
-        delete wdict[pwe][cword]
-        occur.push(cword)
-        document.getElementById("label1").innerText = pwe + '：' + cword
-        if (cword.slice(-1) === 'ー') {
-            cwe = vdict[cword.slice(-2, -1)]
-        } else {
-            cwe = cword.slice(-1)
-        }
-        cwe = rdict[cwe]
-        document.getElementById("label2").innerText = cwe + '：'
-        document.getElementById("entry1").value = ""
-        num += 1
-        numset()
+
+    pwend = sdict[pwend] // 文字を清音に変換
+    used_words.push(pword) // 単語を使用したリストに追加
+
+    // もし機械側の単語選択リストにプレイヤーの単語があれば削除
+    if (wdict[cwend].indexOf(pword)) {
+        delete wdict[cwend][pword]
+    }
+
+    // 勝ち判定
+    if (wdict[pwend].length === 0) {
+        alert('もう言葉を思いつきません\nあなたの勝ちです')
+        refresh()
+        return
+    }
+
+    cword = wdict[pwend][Math.floor(Math.random() * wdict[pwend].length)] // 単語を選ぶ
+    delete wdict[pwend][cword] // 選ばれた単語をリストから削除
+    used_words.push(cword) // 単語を使用されたリストに追加
+
+    document.getElementById("label1").innerText = pwend + '：' + cword // ラベルの文字列を変更
+
+    // 最後の文字が長音符なら一文字前の母音を利用
+    if (cword.slice(-1) === 'ー') {
+        cwend = vdict[cword.slice(-2, -1)]
     } else {
-        alert('確認してください')
+        cwend = cword.slice(-1)
     }
+
+    cwend = sdict[cwend] // 文字を清音に変換
+    document.getElementById("label2").innerText = cwend + '：' // ラベルの文字列を変更
+    document.getElementById("entry1").value = "" // ラベルの文字列を変更
+    num_time += 1 // 回数を進める
+    setNum() // 文字列型に変換してラベルを変更
 }
 
-function numset() {
-    let strnum
-    if (num === 1) {strnum = "１"}
-    else if (num === 2) {strnum = "２"}
-    else if (num === 3) {strnum = "３"}
-    else if (num === 4) {strnum = "４"}
-    else if (num === 5) {strnum = "５"}
-    else if (num === 6) {strnum = "６"}
-    else if (num === 7) {strnum = "７"}
-    else if (num === 8) {strnum = "８"}
-    else if (num === 9) {strnum = "９"}
-    else {strnum = String(num)}
-    document.getElementById("label3").innerText = strnum + "回目"
+// 文字列型に変換してラベルを変更
+function setNum() {
+    let str_time // 文字列型の数字
+    if (num_time === 1) {str_time = "１"}
+    else if (num === 2) {str_time = "２"}
+    else if (num === 3) {str_time = "３"}
+    else if (num === 4) {str_time = "４"}
+    else if (num === 5) {str_time = "５"}
+    else if (num === 6) {str_time = "６"}
+    else if (num === 7) {str_time = "７"}
+    else if (num === 8) {str_time = "８"}
+    else if (num === 9) {str_time = "９"}
+    else {str_time = String(num_time)}
+    document.getElementById("label3").innerText = str_time + "回目"
 }
 
+// データを初期化
 function refresh() {
-    cwe = "リ"
-    cword = "シリトリ"
-    occur = []
-    num = 1
-    document.getElementById("label1").innerText = "始：シリトリ"
-    document.getElementById("label2").innerText = "リ："
-    document.getElementById("entry1").value = ""
-    numset()
+    cword = "シリトリ" // 機械側の単語を初期化
+    cwend = cword.slice(-1) // 機械側の単語の最後の文字を初期化
+    used_words = [] // 使用された単語リストを初期化
+    num_time = 1 // 現在の回数を初期化
+    document.getElementById("label1").innerText = "始：シリトリ" // ラベルの文字列を初期化
+    document.getElementById("label2").innerText = "リ：" // ラベルの文字列を初期化
+    document.getElementById("entry1").value = "" // ラベルの文字列を初期化
+    setNum() // 文字列型に変換してラベルを初期化
 }
